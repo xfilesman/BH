6c3143f8acdb8751e54324169201da8d273474d9
xfilesman <manxfiles@gmail.com>
2021-01-10 13:36:12 +0200

    Update bughunting.sh

--- bughunting.sh
+++ bughunting.sh
@@ -9,19 +9,64 @@ printf "
 "
 
 echo "welcome bughunting"
-
+****************************************************
 echo "this is sublister tool"
+
 git clone https://github.com/aboul3la/Sublist3r.git
-# this is link on github  https://github.com/aboul3la/Sublist3r
-
-
+****************************************************
 echo "this is dir search tool"
 
 git clone https://github.com/maurosoria/dirsearch.git
-# this is link on github
-
-
+****************************************************
 echo "this is webpwn3r tool"
 
 git clone https://github.com/zigoo0/webpwn3r.git
-# this is link on github
+****************************************************
+echo "this tool for phshing social media"
+
+git clone https://github.com/htr-tech/zphisher.git
+****************************************************
+echo ""
+
+git clone https://github.com/Agent00049/ReconCobra.git
+****************************************************
+echo ""
+
+git clone https://github.com/Manisso/fsociety.git
+****************************************************
+echo ""
+
+git clone https://github.com/M4sc3r4n0/Evil-Droid.git
+****************************************************
+
+
+echo""
+git clone https://github.com/smicallef/spiderfoot.git
+****************************************************
+
+echo""
+git clone https://github.com/Und3rf10w/kali-anonsurf.git
+*******************************************************
+
+echo""
+git clone https://github.com/farid007/HashExploit.git
+*****************************************************
+
+echo""
+git clone https://github.com/BishopFox/spoofcheck.git
+*****************************************************
+
+echo""
+git clone https://github.com/mikechabot/smtp-email-spoofer-py.git
+****************************************************************
+
+echo""
+git clone https://github.com/r00t-3xp10it/venom.git
+****************************************************
+
+echo""
+git clone https://github.com/g0tmi1k/msfpc.git
+***********************************************
+
+
+
